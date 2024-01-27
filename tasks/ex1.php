79ddd7141c1e09010890f70ad4f27d0e4ba32485
adamnovacsek <nn53918@gmail.com>
2024-01-27 15:18:39 +0200

    Update ex1.php

--- tasks/ex1.php
+++ tasks/ex1.php
@@ -58,7 +58,7 @@
 
     <h3>3.4 Take a screenshot that confirms your development environment setup and include it as an image in the "ex1.php" file.<h3\>
 
-    <img src= "pictures/phpserver.png" alt="web development environment"><img/>
+    <img src= "./pictures/phpserver.png" alt="web development environment"><img/>
 
 </body>
 </html>
